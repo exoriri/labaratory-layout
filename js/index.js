d9957810937054b66bb4581b02a241092420b862
exoriri <lavrikrom2@gmail.com>
2022-08-20 13:18:29 +0300

    feat: functions

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
-var a = 5;
-let b = 10;
-const c = 15;
-const name = "Yanis";
-const array = ["Привет", b, name];
-const cartButton = document.getElementById("cart-btn");
+let five = 578912; //Number
+let isFiveMoreThanTen = true; //boolean
+let nullVar = null; //undefined
+let undefinedVar = undefined; // null
+let numbers = [3, 4, 6];
 
-cartButton.addEventListener("click", function () {
-  this.style.background = "green";
-});
+const mountains = {
+  names: ['Everest', 'Elbrus', 'Kilimanjaro'],
+  count: 3
+};
+
+function incremetingA() {
+  let a = 5;
+  a += 1;
+  console.log(a)
+};
+
+incremetingA();
+
+function sum(a,b) {
+  console.log(a + b);
+}
+
+console.log(sum(25, 5));
+
+// if (nullVar) {
+//   console.log('Вывелось!')
+// } else {
+//   console.log('Не вывелось')
+// }
